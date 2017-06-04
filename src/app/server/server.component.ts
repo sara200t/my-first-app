6da87e3a844daeaba485c9c6ef498ad3c686018d
Peng <sara.thanit@gmail.com>
2017-06-03 19:08:55 -0600

    finished lecture 32 - directives ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,30 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   serverId = 10;
   serverStatus: string = 'online';
 
-  getServerStatus(){
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    if (this.serverStatus === 'online') {
+      return 'green';
+    } else {
+      return 'red';
+    }
   }
 }
